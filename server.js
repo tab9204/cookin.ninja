55d3b67aa3fbe9218818dbda706224a282fb9490
Trey Borro (Launch Consulting) <v-trborr@microsoft.com>
2021-01-23 15:15:34 -0800

    added server and procfile

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var server = app.listen(process.env.PORT || 3000,function(){console.log("Listening on port 3000!")});
+
+
+//serve the assets
+app.use('/assets', express.static('assets'));
+app.use('/build', express.static('build'));
+app.use('/libraries', express.static('libraries'));
+app.use('/src', express.static('src'));
+app.use('/', express.static('/'));
+
+//serve the html file
+app.get("/",function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get("/manifest.json",function(req,res){
+    res.sendFile(__dirname + '/manifest.json');
+});
+
+app.get("/service-worker.js",function(req,res){
+    res.sendFile(__dirname + '/service-worker.js');
+});
+
+app.get("/offline.html",function(req,res){
+    res.sendFile(__dirname + '/offline.html');
+});
